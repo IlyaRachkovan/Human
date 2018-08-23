efcbe979c2628ed08679e9d25b222e35cebbbb9f
IlyaRachkovan <yettiyork@rambler.ru>
2018-08-23 13:30:40 +0300

    Stop

--- src/CraftHuman.java
+++ src/CraftHuman.java
@@ -1,6 +1,15 @@
 import java.util.Scanner;
 
+
 public class CraftHuman {
+    HumanRedactor humanRedactor = new HumanRedactor();
+
+    private Human [] addHuman (Human [] arr){
+        System.out.println("Please enter value to create a new Human");
+        String value = getNewHuman();
+        arr = humanRedactor.addHuman(arr,human);
+        return arr;
+    }
 
     protected String getNewHuman (){
         String newHuman;
